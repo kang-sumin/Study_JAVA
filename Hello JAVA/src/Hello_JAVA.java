8dc39686cc94689bfd5d136b5154525b54080508
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2023-09-07 22:21:27 +0900

    java study 1day
    baekjoon code

---
+++ Hello JAVA/src/Hello_JAVA.java
@@ -0,0 +1,7 @@
+//백준 2557
+
+public class Hello_JAVA {
+    public static void main(String[] arg){
+        System.out.println("Hello World!");
+    }
+}
---
+++ Hello JAVA/src/Year_Calculation.java
@@ -0,0 +1,13 @@
+//백준 18108
+
+import java.util.Scanner;
+
+public class Year_Calculation {
+    public static void main(String[] arg){
+        Scanner yearscanner = new Scanner(System.in);
+        int year=yearscanner.nextInt();
+
+        System.out.println(year-543);
+
+    }
+}
