807c87a3d697d6f33dad42f1a08788bcdabeca4a
kang-sumin <rkdtnals1117@naver.com>
2024-07-22 11:31:20 +0900

    ãCREATE : new project file

---
+++ Java_Basic/FirstJava/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    /* JDK 역할
+    1) compiler : .java -> .class
+    2) JRE
+    3) JDB : 디버깅
+    * */
+
+    public static void main(String[] args) {
+
+        System.out.println("Our First Porject! :)");
+    }
+}
