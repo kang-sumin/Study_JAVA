ac8c2ae1c6e1949f4fe1d6b7a241148c293e70da
kang-sumin <rkdtnals1117@naver.com>
2024-07-22 11:55:55 +0900

    COMMENT : 설명 주석 추가

--- Java_Basic/FirstJava/src/Main.java
+++ Java_Basic/FirstJava/src/Main.java
@@ -1,9 +1,26 @@
+//클래스
+//public : 제어자, public(공공의, 공통의)
+
 public class Main {
 
     /* JDK 역할
-    1) compiler : .java -> .class
-    2) JRE
-    3) JDB : 디버깅
+    * 1) compiler : .java -> .class
+    * 2) JRE
+    * 3) JDB : 디버깅
+    * */
+
+    /* main 메소드
+    * 자바 프로젝트(앱)는, 제일 먼저 클래스의 main 메소드를 실행시킨다.
+    * = JVM 약속
+    * static : 이 프로그램이 시작될 때 무조건 실행되는 녀석
+    *
+    * <<output>>
+    * void : 메서드의 출력값의 데이터 타입
+    * void : 아무것도 없다 -> 출력이 없다
+    *
+    * <<input>>
+    * String[] args : 매개변수 자리
+    * String[] : input 타입
     * */
 
     public static void main(String[] args) {
