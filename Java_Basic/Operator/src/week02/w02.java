586dcd75b4532229a6030e6bb5d41cbcea5d4a65
kang-sumin <rkdtnals1117@naver.com>
2024-07-23 15:33:45 +0900

    LEARN : 비교연산자

--- Java_Basic/Operator/src/week02/w02.java
+++ Java_Basic/Operator/src/week02/w02.java
@@ -3,26 +3,24 @@ package week02;
 public class w02 {
     public static void main(String[] args) {
         //사칙연산
-        System.out.println(4+2);
-        System.out.println(4-2);
-        System.out.println(4*2);
-        System.out.println(4/2);
-        System.out.println(5%2);
+        System.out.println(4 + 2);
+        System.out.println(4 - 2);
+        System.out.println(4 * 2);
+        System.out.println(4 / 2);
+        System.out.println(5 % 2);
         System.out.println("------------------------");
 
         //우선순위 연산
-        System.out.println(2+2*2); //6
-        System.out.println((2+2)*2); //8
+        System.out.println(2 + 2 * 2); //6
+        System.out.println((2 + 2) * 2); //8
         System.out.println("------------------------");
 
         //변수를 이용한 연산
-        int a=20;
-        int b=10;
+        int a = 20;
+        int b = 10;
         int c;
-        c=a+b;
+        c = a + b;
         System.out.println(c);
-
-
 
 
     }
---
+++ Java_Basic/Operator/src/week02/w03.java
@@ -0,0 +1,17 @@
+package week02;
+
+public class w03 {
+    //비교연산자 예제
+    public static void main(String[] args) {
+
+        System.out.println(10 > 9); //true
+        System.out.println(10 >= 9); //true
+        System.out.println(10 < 9); //false
+        System.out.println(10 <= 9); //false
+        System.out.println(10 == 10); //true
+        System.out.println(10 == 9); //false
+        System.out.println(10 != 10); //false
+        System.out.println(10 != 9); //true
+
+    }
+}
