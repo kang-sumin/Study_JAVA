ee3f7c10026cdeded74d6882bd592c3472eaf6dd
kang-sumin <rkdtnals1117@naver.com>
2024-07-23 16:31:09 +0900

    CREATE : 가위바위보 예제 if문 중첩

---
+++ Java_Basic/Operator/src/week02/w11.java
@@ -0,0 +1,23 @@
+package week02;
+
+public class w11 {
+    //조건문
+    public static void main(String[] args) {
+
+        boolean flag = true;
+
+        if (flag) {
+            System.out.println("값이 true입니다.");
+        } else {
+            System.out.println("값이 false입니다.");
+        }
+
+        if (1 != 1) {
+            System.out.println("true");
+        } else if (1 > 3) {
+            System.out.println("1>3");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
---
+++ Java_Basic/Operator/src/week02/w12.java
@@ -0,0 +1,22 @@
+package week02;
+
+public class w12 {
+    //중첩 if 문
+    public static void main(String[] args) {
+        boolean flag = true;
+        int number = 2;
+
+        if (flag) {
+            System.out.println("flag가 true입니다.");
+            if (number == 1) {
+                System.out.println("1");
+            } else if (number == 2) {
+                System.out.println("2");
+            } else {
+                System.out.println("모름");
+            }
+        } else {
+            System.out.println("flag가 false입니다.");
+        }
+    }
+}
