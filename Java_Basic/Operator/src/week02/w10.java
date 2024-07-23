b018e77669fb1359179616fea3a1e2ef31955c59
kang-sumin <rkdtnals1117@naver.com>
2024-07-23 16:15:21 +0900

    LEARN : 비트연산자

---
+++ Java_Basic/Operator/src/week02/w10.java
@@ -0,0 +1,8 @@
+package week02;
+
+public class w10 {
+    public static void main(String[] args) {
+        System.out.println(3 << 2); //12 : 3*2*2
+        System.out.println(3 << 1); //6 : 3*2
+    }
+}
