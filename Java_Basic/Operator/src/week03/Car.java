429fc4950a8e845f6c59124bf37e24528db098db
kang-sumin <rkdtnals1117@naver.com>
2024-07-24 11:43:48 +0900

    UPDATE : 객체생성

--- Java_Basic/Operator/src/week03/Car.java
+++ Java_Basic/Operator/src/week03/Car.java
@@ -12,17 +12,17 @@ public class Car {
 
     // 1) 고유 데이터 영역
     String company; //자동차 회사
-    String model; //자동차 모델
+    String model = "Gv80"; //자동차 모델
     String color; //자동차 색
     double price; //자동차 가격
 
     // 2) 상태 데이터 영역
     double speed; //자동차 속도, km/h
     char gear; //기어상태(P,R,N,D)
-    boolean lights; //자동차 조명의 상태
+    boolean lights = true; //자동차 조명의 상태
 
     // 3) 객체 데이터 영역
-    Tire tire;
+    Tire tire = new Tire();
     Door door;
     Handle handle;
 
@@ -39,7 +39,9 @@ public class Car {
     //gasPedal
     //input : km/h
     //output : speed
-    double gasPedal(double kmh) {
+    double gasPedal(double kmh, char type) {
+        //가속도 패달을 밟으면 자동으로 기어가 변한다.
+        changeGear(type);
         speed = kmh;
         return speed;
     }
@@ -73,6 +75,13 @@ public class Car {
     //output : X
     void horn() {
         System.out.println("빵빵~~~~~!!!");
+    }
+
+    //자동차의 속도를 구하는 메서드
+    void carSpeeds(double ... speeds){
+        for(double v : speeds){
+            System.out.println("v = "+v);
+        }
     }
 
 
--- Java_Basic/Operator/src/week03/Main.java
+++ Java_Basic/Operator/src/week03/Main.java
@@ -2,23 +2,31 @@ package week03;
 
 public class Main {
     public static void main(String[] args) {
-        Car[] carArray = new Car[3];
-        Car car1 = new Car();
-        car1.changeGear('P');
-        carArray[0]=car1;
-        //System.out.println(car1); //car1이 가지고 있는 주소 출력
+        Car car = new Car();
 
-        Car car2 = new Car();
-        car2.changeGear('N');
-        carArray[1]=car2;
+        //초기값 기본 값 확인 : 초기값을 준 것은 그 값이 들어가고, 아닌 것은 default value 가 set
+        System.out.println("car.model = "+car.model);
+        System.out.println("car.color = "+car.color);
+        System.out.println();
 
-        Car car3 = new Car();
-        car3.changeGear('D');
-        carArray[2]=car3;
+        System.out.println("car.speed ="+car.speed);
+        System.out.println("car.gear ="+car.gear);
+        System.out.println("car.lights ="+car.lights);
+        System.out.println();
 
-        for(Car car:carArray){
-            System.out.println("car.gear = "+ car.gear);
-        }
+        System.out.println("car.tire ="+car.tire);
+        System.out.println("car.door ="+car.door);
+        System.out.println();
+
+        //필드 사용
+        car.color ="blue";
+        car.speed = 100;
+        car.lights =false;
+
+        System.out.println("car.color = "+car.color);
+        System.out.println("car.speed ="+car.speed);
+        System.out.println("car.lights ="+car.lights);
+        System.out.println();
 
 
     }
