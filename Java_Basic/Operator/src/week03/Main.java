38b43927a57a622a785a04fdef4f75ba69efb0b2
kang-sumin <rkdtnals1117@naver.com>
2024-07-24 11:47:47 +0900

    UPDATE : 필드와 메서드

--- Java_Basic/Operator/src/week03/Main.java
+++ Java_Basic/Operator/src/week03/Main.java
@@ -4,29 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car();
 
-        //초기값 기본 값 확인 : 초기값을 준 것은 그 값이 들어가고, 아닌 것은 default value 가 set
-        System.out.println("car.model = "+car.model);
-        System.out.println("car.color = "+car.color);
-        System.out.println();
+        System.out.println("car.gear = "+car.gear);
+        //메서드 호출 및 반환값을 저장
+        double speed = car.gasPedal(100,'D');
+        System.out.println("speed = "+speed);
 
-        System.out.println("car.speed ="+car.speed);
-        System.out.println("car.gear ="+car.gear);
-        System.out.println("car.lights ="+car.lights);
-        System.out.println();
+        boolean lights = car.onOffLight();
+        System.out.println("lights = "+lights);
 
-        System.out.println("car.tire ="+car.tire);
-        System.out.println("car.door ="+car.door);
         System.out.println();
+        System.out.println("car.gear = "+car.gear);
 
-        //필드 사용
-        car.color ="blue";
-        car.speed = 100;
-        car.lights =false;
-
-        System.out.println("car.color = "+car.color);
-        System.out.println("car.speed ="+car.speed);
-        System.out.println("car.lights ="+car.lights);
         System.out.println();
+        car.carSpeeds(100, 80);
+        System.out.println();
+        car.carSpeeds(110, 120, 150);
+
+
 
 
     }
