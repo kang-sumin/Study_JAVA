b36697d0fd084a45d4e1b993f810cab6b54200cc
kang-sumin <rkdtnals1117@naver.com>
2024-07-25 22:04:46 +0900

    [Lv1-1] UPDATE : 양의정수 2개 입력받기

--- Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -1,10 +1,17 @@
 package calculator;
 
+import java.util.*;
+
 public class App {
-
     public static void main(String[] args) {
-        //test
-        System.out.println("안녕하세요");
+        //Scanner 객체 생성
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNum = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNum = sc.nextInt();
+
+        System.out.println(firstNum + "/" + secondNum);
     }
 }
