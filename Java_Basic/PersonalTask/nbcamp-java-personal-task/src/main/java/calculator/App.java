96523777c3d48844fb8faf7997e38599bd29917a
kang-sumin <rkdtnals1117@naver.com>
2024-07-26 19:23:30 +0900

    [Lv1-3] UPDATE : 사칙연산 기호에 따라 연산 진행 후 결과값 반환

--- Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -4,8 +4,9 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        //Scanner 객체 생성
-        Scanner sc = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in); //Scanner 객체 생성
+        int result = 0; //사칙연산 결과값
 
         //양의 정수 2개 입력
         System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -18,7 +19,32 @@ public class App {
         //next()은 Scanner 클래스 메소드로 문자열을 반환하기 때문에 charAt(idx)메소드를 이용해 문자타입으로 변환하여 0번째 문자를 반환
         char operator = sc.next().charAt(0);
 
-        System.out.println(operator);
+
+        //연산 기호 구분 제어문
+        switch (operator) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                if(secondNum==0){
+                    System.out.println("나눈셈 연산에서 분모에 0 이 입력될 수 없습니다.");
+                }else{
+                    result = firstNum / secondNum;
+                }
+                break;
+            default:
+                System.out.println("연산기호는 사칙연산만 입력가능합니다.");
+                break;
+        }
+
+        //연산 결과 출력
+        System.out.println("결과 : " + result);
 
     }
 }
