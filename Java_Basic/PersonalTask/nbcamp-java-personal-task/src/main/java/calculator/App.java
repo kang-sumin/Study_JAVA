598b01831dd357825f10940f76b369f0ad12d0db
kang-sumin <rkdtnals1117@naver.com>
2024-07-28 21:06:56 +0900

    FIX : 0포함 양의정수만 입력되도록 수정

--- Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -8,17 +8,24 @@ public class App {
         Scanner sc = new Scanner(System.in); //Scanner 객체 생성
         LinkedList<Integer> results = new LinkedList<>();
         String exit = "";
-        String remove ="";
-        String inquiry ="";
+        String remove = "";
+        String inquiry = "";
 
-        while(!exit.equals("exit")){
+
+        while (!exit.equals("exit")) {
             int result = 0; //사칙연산 결과값
+            int firstNum = -1;
+            int secondNum = -1;
 
             //양의 정수 2개 입력
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum = sc.nextInt();
+            while (0 > firstNum) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                firstNum = sc.nextInt();
+            }
+            while (0 > secondNum) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                secondNum = sc.nextInt();
+            }
 
             //사칙연산 기호 입력
             System.out.print("사칙연산 기호를 입력하세요: ");
@@ -37,9 +44,9 @@ public class App {
                     result = firstNum * secondNum;
                     break;
                 case '/':
-                    if(secondNum==0){
+                    if (secondNum == 0) {
                         System.out.println("나눈셈 연산에서 분모에 0 이 입력될 수 없습니다.");
-                    }else{
+                    } else {
                         result = firstNum / secondNum;
                     }
                     break;
@@ -54,18 +61,18 @@ public class App {
             //연산 결과값 배열에 저장
             results.add(result);
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            remove=sc.next();
+            remove = sc.next();
 
-            if(remove.equals("remove")){
+            if (remove.equals("remove")) {
                 results.removeFirst();
                 System.out.println("삭제되었습니다.");
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            inquiry=sc.next();
-            if(inquiry.equals("inquiry")){
+            inquiry = sc.next();
+            if (inquiry.equals("inquiry")) {
                 System.out.print("[ ");
-                results.forEach(resultData -> System.out.print("\""+resultData+"\" "));
+                results.forEach(resultData -> System.out.print("\"" + resultData + "\" "));
                 System.out.print("]");
             }
 
