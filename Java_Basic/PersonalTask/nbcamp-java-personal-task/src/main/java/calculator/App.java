a109df7fd334953342688ad345de60f8a6d6a8b4
kang-sumin <rkdtnals1117@naver.com>
2024-07-29 13:25:41 +0900

    [Lv2-6] UPDATE : Calculator instance 생성자 통해 컬렉션 필드 초기화

--- Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ Java_Basic/PersonalTask/nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -6,6 +6,7 @@ public class App {
     public static void main(String[] args) {
         LinkedList<Integer> results = new LinkedList<>();
         Calculator calc = new Calculator(results);
+
         String exit = "";
         String remove = "";
         String inquiry = "";
